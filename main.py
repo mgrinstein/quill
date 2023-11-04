c4ff5dc554dd4ba1a203a9b460302bfdf7e446b8
Eric Grinstein <eric.grinstein@outlook.com>
2023-11-04 17:20:05 +0000

    Download repo

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import requests
 import sys
+import tempfile
+import zipfile
 
 from copy import deepcopy
 from call_claude import call_claude
 from make_prompt import make_prompt
 from read_dir import read_dir
+
+TEMP_DIR = "temp"
 
 BASE_QUESTION = "Create a markdown file that summarizes the following codebase, skipping the preamble: \n"
 GUIDELINES = "Do not include an introductory text for your answer. Just output the .MD file directly \n"
@@ -15,28 +20,39 @@ PROMPT_INSTRUCTIONS = {
 }
 
 
-def main(directory, output_file):
-    codebase = read_dir(directory, output_file)
+def main(repo_path, output_file, branch="master"):
+    temp_dir = tempfile.mkdtemp()
+
+    if repo_path.startswith("http"):
+        print("Downloading repo...")
+        # Repo is a URL. Download it to a temporary directory
+        url = f"{repo_path}/archive/refs/heads/{branch}.zip"
+        r = requests.get(url, allow_redirects=True)
+        # Unzip the downloaded file
+        open(f"{temp_dir}/repo.zip", 'wb').write(r.content)
+        with zipfile.ZipFile(f"{temp_dir}/repo.zip", 'r') as zip_ref:
+            zip_ref.extractall(temp_dir)
+        repo_path = f"{temp_dir}/{repo_path.split('/')[-1]}-{branch}"
+
+    print("Reading repo...")
+    codebase = read_dir(repo_path)
 
     prompt = deepcopy(PROMPT_INSTRUCTIONS)
     prompt["codebase"] = codebase
 
+    print("Calling Claude...")
     prompt = make_prompt(prompt)
-    
     answer = call_claude(prompt)
-    print(answer)
 
     if output_file != "":
         with open(output_file, 'w') as f:
             f.write(answer)
+        print(f"Answer written to {output_file}")
+    else:
+        print(answer)
 
 
 if __name__ == '__main__':
     output_file = "test_readme.md"
     main(sys.argv[1], output_file)
 
-# Base question:
-# Generate a markdown file that summarizes the follwing codebase.
-# It contains the following sections:
-# Introduction
-# 
